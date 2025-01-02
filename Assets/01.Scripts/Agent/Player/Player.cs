f0fa5ac716ecc0ef5f9c6b20ec649e6d073e3d13
Crogen0407 <mymcoding0407@gmail.com>
2025-01-03 01:37:48 +0900

    feat : Player's healthSystem

--- Assets/01.Scripts/Agent/Player/Player.cs
+++ Assets/01.Scripts/Agent/Player/Player.cs
@@ -1,5 +1,6 @@
 using System;
 using Crogen.AgentFSM;
+using Crogen.HealthSystem;
 using Crogen.PowerfulInput;
 using UnityEngine;
 
@@ -7,11 +8,19 @@ public class Player : Agent
 {
     [field:SerializeField] public InputReader InputReader { get; private set; }
     [field:SerializeField] public AgentStatSO AgentStatSO { get; private set; }
+    private HealthSystem _healthSystem;
     
     private void Awake()
     {
         //처음 State는 Idle입니다.
         Initialize<PlayerStateEnum>();
+        
+        //HealthSystem
+        _healthSystem = GetComponent<HealthSystem>();
+        _healthSystem.maxHp = AgentStatSO.health;
+        _healthSystem.Hp = AgentStatSO.health;
+        
+        //Movement
         Movement.Initialize(this);
     }
 }
