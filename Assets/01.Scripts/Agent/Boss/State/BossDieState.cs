ac7001856a253f3026fb26af240194072fb20829
kimtaeyun <sdh230104@sdh.hs.kr>
2025-01-04 04:23:43 +0900

    fix : enemy stat

--- Assets/01.Scripts/Agent/Boss/State/BossDieState.cs
+++ Assets/01.Scripts/Agent/Boss/State/BossDieState.cs
@@ -8,4 +8,21 @@ public class BossDieState : AgentState
     public BossDieState(Agent agentBase, StateMachine stateMachine, string animBoolName) : base(agentBase, stateMachine, animBoolName)
     {
     }
+
+    public override void Enter()
+    {
+        base.Enter();
+        BossMovement bossMovement = _agentBase.GetComponent<BossMovement>();
+        bossMovement.enabled = false;
+    }
+
+    public override void UpdateState()
+    {
+        base.UpdateState();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
 }
