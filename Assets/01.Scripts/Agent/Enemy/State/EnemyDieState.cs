5e7433317320e18c3851582cb5ff0ca9c89dba29
Crogen0407 <mymcoding0407@gmail.com>
2025-01-04 07:40:37 +0900

    fix : Enemy Die State

--- Assets/01.Scripts/Agent/Enemy/State/EnemyDieState.cs
+++ Assets/01.Scripts/Agent/Enemy/State/EnemyDieState.cs
@@ -8,12 +8,17 @@ public class EnemyDieState : AgentState
     public EnemyDieState(Agent agentBase, StateMachine stateMachine, string animBoolName) : base(agentBase, stateMachine, animBoolName)
     {
     }
-    public override void Enter()
+    public override void Enter()    
     {
         base.Enter();
         EnemyMovement enemyMovement = _agentBase.GetComponent<EnemyMovement>();
         enemyMovement.enabled = false;
-        SoundManager.Instance.PlaySFX("EnemyDieSound"); }
+        SoundManager.Instance.PlaySFX("EnemyDieSound");
+        Debug.Log("dfsf");
+        (_agentBase as Enemy).isDead = true;
+        (_agentBase as Enemy).
+            DestroySelf();
+    }
 
     public override void Exit()
     {
